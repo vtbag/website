1777f5376ccbf6c7ed7491cdd4a7eab2adc4c5f3
Martin Trapp <94928215+martrapp@users.noreply.github.com>
2024-07-29 07:32:51 +0200

    updates

--- playwright.config.ts
+++ playwright.config.ts
@@ -37,17 +37,6 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
   ],
 
   /* Run your local dev server before starting the tests */
