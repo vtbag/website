f227b2b8f6d8695343190fc3b83fce2599f74be3
Martin Trapp <94928215+martrapp@users.noreply.github.com>
2024-11-19 18:08:26 +0100

    improved hide-and-seek

--- bin/og.sh
+++ bin/og.sh
@@ -1,5 +1,25 @@
 #! /bin/bash
 
+
+subtract_lists() {
+  echo "$1" | tr ' ' '\n' | grep -Fxv -f <(echo "$2"|tr ' ' '\n')
+}
+
+htmls=$(find  dist -type f -name "*.html" | sort)
+
+desc=$(grep -l '"og:description"' $htmls)
+no_desc=$(subtract_lists "$htmls" "$desc")
+
+echo $no_desc
+exit
+done=$(grep -o '"og:image" content="[^"]*"' $htmls|grep -v "https://vtbag.dev/social.png" | col1 :)
+
+done+=" dist/index.html"
+done=$(echo "$done" | tr ' ' '\n' | sort)
+todo=$(subtract_lists "$htmls" "$done")
+echo $todo
+exit
+
 find dist -type f -name "*.html" -exec grep -H  -o '"og:url" content="[^"]*"' {} \; | sed -e 's/:/ /' -e 's/=/ /' -e 's/"//g' -e 's|og:url content https://vtbag.dev||' > pages
 
 awk < pages '{print "test(\"" $2 "\", async ({ page }) => {await shoot(page, \"" $2 "\")});"; system("mkdir -p public/" $2);}' | sort
---
+++ src/components/cutter.ts
@@ -0,0 +1,37 @@
+export function cutSVG(element: Element, cutters:{element:HTMLElement,inset: string}[]) {
+  if (!element) return;
+  const masks = ['<rect width="100%" height="100%" fill="white" />'];
+  const outerRect = element.getBoundingClientRect();
+  cutters.forEach((cutter) => {
+    const inner = cutter.element;
+    const insets = cutter.inset.split(" ").map(parseFloat);
+    for (let i = 0; i < 4; i++) {
+      const len = insets.length;
+      insets[i] = isNaN(insets[i] ?? 0)
+        ? 0
+        : (insets[len === 3 && i === 3 ? 2 : i % len] ?? 0);
+    }
+    const innerRect = inner.getBoundingClientRect();
+    const innerStyle = getComputedStyle(inner);
+
+    const left = parseFloat(innerStyle.borderLeftWidth);
+    innerRect.width -=
+      left +
+      parseFloat(innerStyle.borderRightWidth) +
+      insets[1]! +
+      insets[3]!;
+    innerRect.x += left + insets[3]!;
+    const top = parseFloat(innerStyle.borderTopWidth);
+    innerRect.height -=
+      top +
+      parseFloat(innerStyle.borderBottomWidth) +
+      insets[0]! +
+      insets[2]!;
+    innerRect.y += top + insets[0]!;
+    masks.push(
+      `<rect x="${innerRect.x - outerRect.x}" y="${innerRect.y - outerRect.y}" width="${innerRect.width}" height="${innerRect.height}" fill="black" />`
+    );
+  });
+  const svg = `<svg xmlns="http://www.w3.org/2000/svg" width="${outerRect.width}" height="${outerRect.height}">${masks.join("")}</svg>`;
+  return svg;
+}
